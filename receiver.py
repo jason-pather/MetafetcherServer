3daa58d14e687542ff8d41809bd4a3420e558ca1
jason-pather <jason.pather@live.com>
2014-07-10 16:45:43 +1200

    test

--- receiver.py
+++ receiver.py
@@ -10,18 +10,19 @@ from flask import request
 
 app = Flask(__name__)
 
-global counter = 0;
+counter = 0
 
 @app.route('/receiver', methods=['POST'])
 def receiver():
 
+	global counter
 	if request.headers['Content-Type'] == 'text/plain':
-		counter++;
-		return "text received, counter is "+counter
+		counter+=1
+		return "text received, counter is "+ str(counter)
 	else:
-		counter++;
-		return "other, counter is "+counter
+		counter+=1
+		return "other, counter is "+ str(counter)
 
 @app.route('/receiver', methods=['GET'])
 def other():
-	return "received GET request, counter is "+counter
+	return "received GET request, counter is " + str(counter)
