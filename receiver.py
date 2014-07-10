bf68af4776eb5a0b3f2580affcc2b3e93814b595
jason-pather <jason.pather@live.com>
2014-07-10 12:35:34 +1200

    fix

--- receiver.py
+++ receiver.py
@@ -1,8 +1,14 @@
-import os
+#Copyright Jon Berg , turtlemeat.com
+
+import string,cgi,time
+from os import curdir, sep
+from BaseHTTPServer import BaseHTTPRequestHandler
 from flask import Flask
+#import pri
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'receiver'
+@app.route('/receiver', methods=['POST'])
+def receiver():
+
+	return 'receiver'
