62564e0347d190c4fe15d09ecf0d665e0f94faae
jason-pather <jason.pather@live.com>
2014-07-25 17:25:00 +1200

    sql-alchemy

--- receiver.py
+++ receiver.py
@@ -38,7 +38,8 @@ cur = None
 
 def storeCallLog(callLog):
 	global cur
-	query = "INSERT INTO calllogs VALUES (\"calltype\", \"name\", 100, 100, true, false, \"contactNo\", \"callType\");"
+	query = "INSERT INTO test VALUES (\"testname\");"
+	# query = "INSERT INTO calllogs VALUES (\"calltype\", \"name\", 100, 100, true, false, \"contactNo\", \"callType\");"
 	cur.execute(query)
 	return "call Log"
 
